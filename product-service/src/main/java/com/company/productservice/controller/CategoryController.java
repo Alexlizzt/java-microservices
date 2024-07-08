c1f91e1b49844e4dbc73c13f8f76f73f60e3cbf0
Alelizzt <alejandrolistc@hotmail.com>
2024-07-08 14:59:37 -0500

    Implementing RefreshScope

---
+++ product-service/src/main/java/com/company/productservice/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.company.productservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@Getter
+@Setter
+@RefreshScope // Refresh all @value data after /actuator/refresh
+public class CategoryController {
+
+    @Value("${app.prop1}")
+    private String prop1;
+
+    @GetMapping("/prop")
+    public String getProp1(){
+        return this.prop1;
+    }
+}
