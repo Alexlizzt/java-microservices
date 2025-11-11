0200f87bdd35abdbade8754f474461ef1fbb55ae
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2025-11-11 16:53:51 -0500

    Se configura api-gateway para conectarse a keycloak y establecer login

--- api-gateway/src/main/java/com/company/api_gateway/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/company/api_gateway/SecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.company.api_gateway;
 
+import java.net.URI;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.savedrequest.WebSessionServerRequestCache;
+import org.springframework.web.server.WebSession;
+import org.springframework.http.HttpStatus;
 
 
 @Configuration
@@ -14,9 +20,15 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-        httpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-        httpSecurity.csrf().disable();
+        httpSecurity
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().authenticated()
+            )
+            // Configuración de login OAuth2
+            .oauth2Login(
+                Customizer.withDefaults()
+            );
         return httpSecurity.build();
     }
 }
